c6f9046dba20bec0746d599e6fc209e25d45ef8d
jx <954860224@qq.com>
2020-06-18 18:28:31 +0800

    recover from preorder started!!

---
+++ recoverFromPreorder/main.go
@@ -0,0 +1,29 @@
+/*1028. Recover a Tree From Preorder Traversal
+We run a preorder depth first search on the root of a binary tree.
+
+At each node in this traversal, we output D dashes (where D is the depth of this node), then we output the value of this node.  (If the depth of a node is D, the depth of its immediate child is D+1.  The depth of the root node is 0.)
+
+If a node has only one child, that child is guaranteed to be the left child.
+
+Given the output S of this traversal, recover the tree and return its root.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/recover-a-tree-from-preorder-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func recoverFromPreorder(S string) *TreeNode {
+
+}
+
+func main() {
+
+}
